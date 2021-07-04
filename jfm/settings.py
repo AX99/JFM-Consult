9a00a85f98e2c0a91c028fb3caa61eb27611a71a
AX99 <rapha.mensah@gmail.com>
2021-07-04 12:34:42 +0100

    .env

--- jfm/settings.py
+++ jfm/settings.py
@@ -1,9 +1,14 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Connect the path with your '.env' file name
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+# load_dotenv(find_dotenv())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
